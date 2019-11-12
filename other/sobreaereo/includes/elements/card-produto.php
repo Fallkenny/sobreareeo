768c4fb17a8c74c2491d2a76fe8c6bb2e3d6d5f5
Felipe Fischer <ffsilva8@ucs.br>
2019-11-11 21:23:30 -0300

    a

--- other/sobreaereo/includes/elements/card-produto.php
+++
@@ -1,7 +0,0 @@
-<a href="<?= $link ?>" class="card-produto">
-    <div class="card-produto__foto">
-        <img src="<?= $foto ?>" alt="<?= $titulo?> | Sobreaéreo">
-    </div>
-    <h3 class="card-produto__titulo"><?= $titulo ?></h3>
-    <p class="card-produto__preco"><?= $preco ?></p>
-</a>
